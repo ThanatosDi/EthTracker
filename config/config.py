ce8a157945e899cda6e13a38148dcb0913a38909
ThanatosDi <yykkold55tw@gmail.com>
2022-02-09 00:19:10 +0800

    add client id

--- config/config.py
+++ config/config.py
@@ -1,5 +1,6 @@
 COMMAND_PREFIX="$"
 DISCORD_TOKEN = "{put your discord bot token here}"
+CLIENT_ID = "{put your discord bot client id here}"
 # PRICE_API = "https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD"
 ETH_API = "https://ethgas.watch/api/gas"
 
